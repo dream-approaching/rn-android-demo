2fa38bc484235c8684b940196ef45d22db619f8d
longzi <891661163@qq.com>
2019-04-15 18:26:24 +0800

    测试react-navigation 2.18.3

--- tab1.js
+++ tab1.js
@@ -1,25 +1,4 @@
-import React from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry} from 'react-native';
+import Page from './src/page1'
 
-class HelloWorld extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.hello}>Hello, Worlds tab1 bundle</Text>
-      </View>
-    );
-  }
-}
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center'
-  },
-  hello: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  }
-});
-
-export default AppRegistry.registerComponent('MyReactNativeApp', () => HelloWorld);
+export default AppRegistry.registerComponent('MyReactNativeApp', () => Page);
