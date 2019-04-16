fcfd5fa52f9fea677250a12ba7edde5d09650e91
longzi <891661163@qq.com>
2019-04-16 15:06:54 +0800

    修改rnpm的指向，以此修复react-native link命令失败的问题

--- android/app/src/main/java/com/example/myapplication/MainActivity.java
+++ android/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,15 +8,21 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 
 import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactPackage;
 import com.facebook.react.ReactRootView;
 import com.facebook.react.common.LifecycleState;
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 import com.facebook.react.shell.MainReactPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements DefaultHardwareBackBtnHandler {
     private final int OVERLAY_PERMISSION_REQ_CODE = 1;
     private ReactRootView mReactRootView;
     private ReactInstanceManager mReactInstanceManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +33,9 @@ public class MainActivity extends AppCompatActivity implements DefaultHardwareBa
                 .setCurrentActivity(this)
                 .setBundleAssetName("one.bundle")
                 .setJSMainModulePath("index")
-                .addPackage(new MainReactPackage())
+                .addPackages(Arrays.<ReactPackage>asList(
+                        new MainReactPackage(),
+            new RNDeviceInfo()))
                 .setUseDeveloperSupport(BuildConfig.DEBUG)
                 .setInitialLifecycleState(LifecycleState.RESUMED)
                 .build();
--- src/page1.js
+++ src/page1.js
@@ -1,19 +1,33 @@
 import React from "react";
-import { View, Text,  Button } from "react-native";
-import { createStackNavigator } from 'react-navigation';
-import * as Animatable from 'react-native-animatable';
-
+import { View, Text, Button } from "react-native";
+import { createStackNavigator } from "react-navigation";
+import * as Animatable from "react-native-animatable";
+import DeviceInfo from "react-native-device-info";
 
 class HomeScreen extends React.Component {
+  componentDidMount() {
+    console.log(
+      "%cgetBrand():",
+      "color: #0e93e0;background: #aaefe5;",
+      DeviceInfo.getBrand()
+    );
+  }
+
   render() {
-    console.log('HomeScreen render')
+    console.log("HomeScreen render");
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Home Screen</Text>
-        <Animatable.Text animation="slideOutLeft" iterationCount={1} direction="alternate">Up and down you go</Animatable.Text>
+        <Animatable.Text
+          animation="slideOutLeft"
+          iterationCount={1}
+          direction="alternate"
+        >
+          Up and down you go
+        </Animatable.Text>
         <Button
           title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
+          onPress={() => this.props.navigation.navigate("Details")}
         />
       </View>
     );
@@ -22,9 +36,9 @@ class HomeScreen extends React.Component {
 
 class DetailsScreen extends React.Component {
   render() {
-    console.log('DetailsScreen render')
+    console.log("DetailsScreen render");
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Details Screen</Text>
       </View>
     );
@@ -34,10 +48,10 @@ class DetailsScreen extends React.Component {
 const RootStack = createStackNavigator(
   {
     Home: HomeScreen,
-    Details: DetailsScreen,
+    Details: DetailsScreen
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: "Home"
   }
 );
 
